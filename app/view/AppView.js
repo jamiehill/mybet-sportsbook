da037f1903d0597beaa359211e418be23ded7db7
Jamie Hill <jamie@jamies-air.betclicstage.com>
2015-06-09 13:36:49 +0100

    updated view logic

--- app/view/AppView.js
+++ app/view/AppView.js
@@ -1,10 +1,6 @@
 import Marionette from 'marionette';
 import template from 'app/view/AppView.tpl.html!text'
 
-
-export default class AppView extends Marionette.ItemView {
-    constructor(...rest) {
-    super(...rest);
-        this.template = template;
-}
-};
+export default Marionette.ItemView.extend({
+    template: template
+});
