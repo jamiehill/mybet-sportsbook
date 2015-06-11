3601593e9199dd6e817b5857ea1bde2c50bd6237
jamie <jamie@tiltdigital.com>
2015-06-11 11:21:02 +0100

    now working with karma test integration

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 import Marionette from 'marionette';
 import AppLayout from 'app/view/app-layout';
+
 // shims marionette with backbone/jquery
 import 'marionette-shim';
-
 
 var App = new Marionette.Application();
 App.addInitializer(function() {
 
+	// attach main layout on start
 	App.layout = new AppLayout({el: '#main'});
 	App.layout.render();
 
