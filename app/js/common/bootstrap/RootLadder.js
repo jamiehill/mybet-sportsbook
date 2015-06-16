ea54c2dd596b030ea334d21a5d5e093ec8556450
jamie <jamie@tiltdigital.com>
2015-06-16 23:57:37 +0100

    react integrated into marionette views

--- app/js/common/bootstrap/RootLadder.js
+++ app/js/common/bootstrap/RootLadder.js
@@ -1,11 +1,9 @@
 import DeferredBase from '../core/Deferred';
 
-
 export default class RootLadder extends DeferredBase {
 	constructor(name) {
 		super('RootLadder');
 	}
-
 
 	initialize() {
 		ctx.get('oddsFactory')
