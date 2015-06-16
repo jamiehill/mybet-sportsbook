13d51e1a41b874a4f1da6c782004848551039889
jamie <jamie@tiltdigital.com>
2015-06-16 23:52:10 +0100

    react integrated into marionette views

--- app/js/common/react/ReactView.js
+++ app/js/common/react/ReactView.js
@@ -1,6 +1,5 @@
 import Decorator from '../../common/react/ReactDecorator';
 
-
 export default Marionette.View.extend(Decorator).extend({
 	// returns the react component for this view,
 	// with necessary data applied to its constructor
