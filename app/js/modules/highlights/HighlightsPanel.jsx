0442d3738dc5b40d2652c962d330172adede188d
jamie <jamie@tiltdigital.com>
2015-06-16 23:50:50 +0100

    react integrated into marionette views

--- app/js/modules/highlights/HighlightsPanel.jsx
+++ app/js/modules/highlights/HighlightsPanel.jsx
@@ -3,17 +3,14 @@ import React from 'react';
 
 export default React.createClass({
 	render: function() {
-		var rows = [];
-		_.times(12, function() {
-			rows.push()
-		});
 		return (
 			<div className="inner-cell">
 				<h4>Highlights</h4>
 				<div className="table regular-table">
 					{this.props.children}
 				</div>
-			</div>)
+			</div>
+		)
 	}
 
 });
